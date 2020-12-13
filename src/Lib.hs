4e486a454e037e5e64b937b80c19411963e9a119
Jared Goguen <jared.goguen@gmail.com>
2020-12-13 17:10:13 -0500

    Rough, but functional implementation of showBoard function.

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,4 +3,36 @@ module Lib
     ) where
 
 showBoard :: IO ()
-showBoard = putStrLn "board"
+showBoard = print board
+
+data Team = Red | Yellow | Empty
+type Disc = Team
+type Slot = Disc
+type Row = [Slot]
+type Board = [Row]
+
+redDisc :: Disc
+redDisc = Red
+
+yellowDisc :: Disc
+yellowDisc = Yellow
+
+emptyDisc :: Disc
+emptyDisc = Empty
+
+redSlot :: Slot
+redSlot = redDisc
+
+yellowSlot :: Slot
+yellowSlot = yellowDisc
+
+emptySlot :: Slot
+emptySlot = emptyDisc
+
+board :: Board
+board = [[emptySlot, emptySlot, emptySlot], [emptySlot, emptySlot, emptySlot], [emptySlot, redSlot, yellowSlot]]
+
+instance Show Team where
+    show Red = "R"
+    show Yellow = "Y"
+    show Empty = " "
